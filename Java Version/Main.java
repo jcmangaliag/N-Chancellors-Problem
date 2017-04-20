5cae69070cdc37fb30a64d96f467ef914b6362cd
vince <jbliwag1@up.edu.ph>
2017-04-20 18:26:12 +0800

    Initial UI (not final haha)

--- Java Version/Main.java
+++ Java Version/Main.java
@@ -5,6 +5,12 @@ public class Main {
 	private static JFrame mainFrame;
 	private static CardLayout cl;
 	private static JPanel titlePanel;
+	private static JPanel configPanel;
+	private static JPanel solverPanel;
+
+	private static JButton startButton;
+	private static JButton quitButton;
+
 
 	public static void main(String[] args) {
 		mainFrame = new JFrame("N-Chancellors Problem");
@@ -14,11 +20,25 @@ public class Main {
 
 		Container c = mainFrame.getContentPane();
 		c.setPreferredSize(new Dimension(640,480));
+		
 		cl = new CardLayout();
 		c.setLayout(cl);
 
 		titlePanel = new JPanel();
+		startButton = new JButton("START");
+		quitButton = new JButton("QUIT");
+		titlePanel.add(startButton);
+		titlePanel.add(quitButton);
+
 		titlePanel.setLayout(new FlowLayout());
+		configPanel = new JPanel();
+		solverPanel = new JPanel();
+
+		c.add(titlePanel, "title");
+		c.add(configPanel, "config");
+		c.add(solverPanel, "solver");
+
+		cl.show(c,"title");
 
 		mainFrame.pack();
 		mainFrame.setLocationRelativeTo(null);
